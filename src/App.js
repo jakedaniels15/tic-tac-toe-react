bb151b8070ec1a84480cd6117c305b6cac22814e
jakedaniels15 <jake.daniels15@hotmail.co.uk>
2025-08-11 11:59:51 +0100

    winner function added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import {useState} from 'react'
+import { useState } from "react";
 
 function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
-      {value}</button>
+      {value}
+    </button>
   );
 }
 
@@ -11,25 +12,33 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick(i){
-    if(squares[i]){
-      return
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
     }
-    const nextSquares = squares.slice()
-    if(xIsNext){
-      nextSquares[i] = 'X'
-    }else{
-      nextSquares[i] = 'O'
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
     }
-    setSquares(nextSquares)
-    setXIsNext(!xIsNext)
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
+     const winner = calculateWinner(squares);
+    let move;
+    if (winner) {
+      move = "Winner: " + winner;
+    } else {
+      move = "Next player: " + (xIsNext ? "X" : "O");
+    }
   return (
     <>
+      <div className="status">{move}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
@@ -54,7 +63,7 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6]
+    [2, 4, 6],
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
